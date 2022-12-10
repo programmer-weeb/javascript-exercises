0a107279ceeaa664395940f9fffe318554914b9b
Ahmed Melouk <84931803+programmer-weeb@users.noreply.github.com>
2022-12-10 21:07:14 +0200

    repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,8 @@
-const repeatString = function() {
-
+const repeatString = function(string, times) {
+    for (let i = 1; i < times; i++) {
+        string += string
+    }
+    return string
 };
 
 // Do not edit below this line
