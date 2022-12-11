98b6972048e7aced0bf8bf4bccefd4f343e80fdd
Ahmed Melouk <84931803+programmer-weeb@users.noreply.github.com>
2022-12-11 17:29:11 +0200

    Make repeatString() pass all tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,8 +1,14 @@
-const repeatString = function(string, times) {
-    for (let i = 1; i < times; i++) {
-        string += string
-    }
-    return string
+const repeatString = function (string, times) {
+    let newStr = string
+	if (times == 0) return "";
+	if (times == -1) return "ERROR";
+	if (times == 1) return string;
+	else {
+		for (let i = 1; i < times; i++) {
+			newStr = newStr + string
+		}
+        return newStr;
+	}
 };
 
 // Do not edit below this line
