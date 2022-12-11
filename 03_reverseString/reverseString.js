b5ccd0694914f06fbc199aebdfd0f007e6d8a237
Ahmed Melouk <84931803+programmer-weeb@users.noreply.github.com>
2022-12-11 18:55:20 +0200

    Finish reverseString() with all tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,16 @@
-const reverseString = function() {
+const reverseString = function (enteredString) {
+	let arrOfChars = enteredString.split("");
+	let revArrOfChar = [];
 
+	for (let i = 0; i < arrOfChars.length; i++) {
+		revArrOfChar[i] = arrOfChars[arrOfChars.length - i - 1];
+	}
+
+	return revArrOfChar.join("");
 };
+// get string from user
+// put chars of user string from back to the new arr of chars
+// turn the new arr of char to string
 
 // Do not edit below this line
 module.exports = reverseString;
