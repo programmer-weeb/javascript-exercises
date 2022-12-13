d276752710b743d8e7967c8fc147515255d3eb9d
Ahmed Melouk <84931803+programmer-weeb@users.noreply.github.com>
2022-12-13 13:17:37 +0200

    Finish sumAll()
    sumAll(1, 4) -> 1+2+3+4 -> 10

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,14 +1,12 @@
-const sumAll = function(num1, num2) {
-    
-    let from = num1 < num2 ? num1: num2;
-    let to = num1 > num2 ? num1: num2;
-    let sum = 0;
+const sumAll = function (num1, num2) {
+	let from = num1 < num2 ? num1 : num2;
+	let to = num1 > num2 ? num1 : num2;
+	let sum = 0;
 
-    for (let i = from; i <= to; i++) {
-        
-        sum = sum + i;
-    }
-    return sum;
+	for (let i = from; i <= to; i++) {
+		sum = sum + i;
+	}
+	return sum;
 };
 
 // Do not edit below this line
