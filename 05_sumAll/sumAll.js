7378153bc3daad590a95881ca67cc4a72562699d
Ahmed Melouk <84931803+programmer-weeb@users.noreply.github.com>
2022-12-13 13:18:52 +0200

    Finish sumAll() with error message

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,6 @@
 const sumAll = function (num1, num2) {
+    if (!Number.isInteger(num1) || !Number.isInteger(num2)) return "ERROR";
+	if (num1 < 0 || num2 < 0) return "ERROR";
 	let from = num1 < num2 ? num1 : num2;
 	let to = num1 > num2 ? num1 : num2;
 	let sum = 0;
