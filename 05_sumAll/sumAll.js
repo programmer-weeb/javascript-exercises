29a91ccc872efed13d601e73418403aa0e816abd
Ahmed Melouk <84931803+programmer-weeb@users.noreply.github.com>
2022-12-13 12:55:37 +0200

    sumAll without error message

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    
+    let from = num1 < num2 ? num1: num2;
+    let to = num1 > num2 ? num1: num2;
+    let sum = 0;
 
+    for (let i = from; i <= to; i++) {
+        
+        sum = sum + i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
