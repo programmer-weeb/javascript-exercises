df41224878c6602b33cc30d431894661938c359a
Ahmed Melouk <84931803+programmer-weeb@users.noreply.github.com>
2022-12-13 09:47:36 +0200

    Finish removeFromArray()
    did it from solution branch 😢

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function (...args) {
+	let enteredArr = args[0];
+	let newArrWithout = [];
 
+	enteredArr.forEach(e => {
+        if (!args.includes(e)) {
+            newArrWithout.push(e)
+        }
+    });
+
+	return newArrWithout;
 };
 
 // Do not edit below this line
